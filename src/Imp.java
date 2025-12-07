a40d2e76207591c37eccde5d9bb40da85b4025c9
adonayaraia02@gmail.com <adonayaraia02@gmail.com>
2025-12-07 15:19:48 -0500

    kruskal done

--- src/Imp.java
+++ src/Imp.java
@@ -40,8 +40,44 @@ public class Imp {
                 "Prim on nVertex = " + nVertex + " took " + elapsed + " milliseconds, MST edges = " + mst.size());
     }
 
+    private static KruskalGraph RandomGraphKruskal(int nVertex, int edgesPerVertex) {
+        KruskalGraph graphed = new KruskalGraph(nVertex);
+        Random random = new Random();
+
+        // create a connected graph
+        for (int i = 0; i < nVertex - 1; i++) {
+            int w = random.nextInt(100) + 1;
+            graphed.addEdge(i, i + 1, w);
+        }
+        // create extra random edges
+        for (int v = 0; v < nVertex; v++) {
+            for (int k = 0; k < edgesPerVertex; k++) {
+                int u = random.nextInt(nVertex);
+                if (u != v) {
+                    int w = random.nextInt(100) + 1;
+                    graphed.addEdge(v, u, w);
+                }
+            }
+        }
+
+        return graphed;
+    }
+
+    private static void kruskalTimeComplexity(int nVertex) {
+        int edgesPerVertex = 2;
+        KruskalGraph graphed = RandomGraphKruskal(nVertex, edgesPerVertex);
+
+        long start = System.currentTimeMillis();
+        KruskalAlgorithm[] mst = graphed.kruskal();
+        long end = System.currentTimeMillis();
+
+        long elapsed = end - start;
+        System.out.println(
+                "Kruskal on nVertex = " + nVertex + " took " + elapsed + " milliseconds, MST edges = " + mst.length);
+    }
+
     public static void main(String[] args) {
-    //Prims
+        // Prims
         Graph graph = new Graph(6);
 
         graph.addEdge(0, 1, 8);
@@ -59,10 +95,11 @@ public class Imp {
         primTimeComplexity(100);
         primTimeComplexity(1000);
         primTimeComplexity(10000);
-    
-//Kruskal
+
+        // Kruskal
         System.out.println("\nKruskal's Algorithm:\n");
 
+        System.out.println("\nKruskal's Algorithm on graph with 6 vertices):");
         KruskalGraph kruskalGraph = new KruskalGraph(6);
 
         kruskalGraph.addEdge(0, 1, 8);
@@ -75,5 +112,10 @@ public class Imp {
         KruskalAlgorithm[] mstKruskal = kruskalGraph.kruskal();
 
         kruskalGraph.printMST(mstKruskal);
+
+        System.out.println("\nTiming Kruskal's Algorithm on larger graphs");
+        kruskalTimeComplexity(100);
+        kruskalTimeComplexity(1000);
+        kruskalTimeComplexity(10000);
     }
 }
